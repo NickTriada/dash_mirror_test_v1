860ef94374e465d2b0eaee81ac6cf9f0e2872309
NickTriada <NickTriada@gmail.com>
2023-07-12 09:00:05 +0000

    some borders added and cells descriptions

--- app/features/Grid.tsx
+++ app/features/Grid.tsx
@@ -6,9 +6,9 @@ export default class MyFirstGrid extends React.Component {
     render() {
         // layout is an array of objects, see the demo for more complete usage
         const layout = [
-            { i: "a", x: 0, y: 0, w: 1, h: 2, static: true },
-            { i: "b", x: 1, y: 0, w: 3, h: 2, minW: 2, maxW: 4 },
-            { i: "c", x: 4, y: 0, w: 1, h: 2 }
+            { i: "a", x: 0, y: 0, w: 3, h: 4, static: false },
+            { i: "b", x: 3, y: 0, w: 3, h: 4, minW: 2, maxW: 3, maxH: 6},
+            { i: "c", x: 6, y: 0, w: 3, h: 4, minW: 2, maxW: 3, maxH: 6 }
         ];
         return (
             <GridLayout
@@ -18,9 +18,28 @@ export default class MyFirstGrid extends React.Component {
                 rowHeight={30}
                 width={1200}
             >
-                <div key="a">a</div>
-                <div key="b">b</div>
-                <div key="c">c</div>
+                
+                <div 
+                style={{
+                    borderColor: "#FF0000",
+                    borderStyle: "solid",
+                    borderWidth: "1px",
+                  }}
+                   key="a"> CELL FOR CLOCK</div>
+                <div 
+                    style={{
+                    borderColor: "#FF0000",
+                    borderStyle: "solid",
+                    borderWidth: "1px",
+                  }}
+                  key="b">CELL FOR CALENDAR</div>
+                <div 
+                    style={{
+                    borderColor: "#FF0000",
+                    borderStyle: "solid",
+                    borderWidth: "1px",
+                  }}
+                  key="c">CELL FOR NEWS</div>
             </GridLayout>
         );
     }
--- app/routes/dash.tsx
+++ app/routes/dash.tsx
@@ -2,7 +2,7 @@ import Grid from "~/features/Grid";
 
 export default function DashboardPage() {
     return <>
-        <h1>Title hahaha</h1>
+        <h1>DASH MAIN PAGE V0 Test 7-12-23</h1>
         <Grid />
     </>
 }
